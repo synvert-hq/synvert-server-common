1912832c07ec16bfeba3caab5e804243074e5784
Richard Huang <flyerhzm@gmail.com>
2023-02-04 15:08:11 +0800

    add formatCommandResult function

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,4 +1,4 @@
-import { runShellCommand } from "../src/index";
+import { formatCommandResult, runShellCommand } from "../src/index";
 
 describe("runShellCommand", () => {
   it("runs shell command correctly", async () => {
@@ -11,5 +11,19 @@ describe("runShellCommand", () => {
     const result = await runShellCommand("ohce", ["hello world"]);
     expect(result.stdout).toEqual("");
     expect(result.stderr).toContain("command not found: ohce");
+  });
+});
+
+describe("formatCommandResult", () => {
+  it("formats with empty stderr", () => {
+    expect(formatCommandResult({ stdout: "hello world", stderr: "" })).toEqual({ output: "hello world", error: undefined });
+  });
+
+  it("formats with stderr", () => {
+    expect(formatCommandResult({ stdout: "", stderr: "hello world" })).toEqual({ output: "", error: "hello world" });
+  });
+
+  it("formats with warning", () => {
+    expect(formatCommandResult({ stdout: "hello world", stderr: "warning: hello world" })).toEqual({ output: "hello world", error: undefined });
   });
 });
